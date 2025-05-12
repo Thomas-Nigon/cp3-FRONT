efe8b1dac9c0f4a55afd62aaf9ed28fbfa9810b5
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2025-05-12 11:25:28 +0200

    checkpoint done

--- frontend/src/pages/Country.tsx
+++ frontend/src/pages/Country.tsx
@@ -1,20 +1,25 @@
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { GET_COUNTRY } from "../api/example";
+import "./Country.css";
 
 export const Country = () => {
   const { id } = useParams();
   const { data, loading, error } = useQuery(GET_COUNTRY, {
     variables: { countryId: parseInt(id || "0") },
   });
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+
+  if (loading) return <div className="loading">Loading country details...</div>;
+  if (error) return <div className="error">Error: {error.message}</div>;
+
   return (
-    <div>
-      <h1>Country with id:{id}</h1>
-      <p>{data?.country.name}</p>
-      <p>{data?.country.emoji}</p>
-      <p>{data?.country.continent.name}</p>
+    <div className="country-countainer">
+      <div>
+        <h1>{data?.country.name}</h1>
+        <p>{data?.country.emoji}</p>
+        <p>Country Code: {data?.country.code}</p>
+        <p>Continent: {data?.country.continent?.name || "Not specified"}</p>
+      </div>
     </div>
   );
 };
